47f39c165b6a98e1541407f7f3d2d9f1509ac34b
Iago Vinicius Russi Novaes <34945729+iago010@users.noreply.github.com>
2023-09-19 18:31:25 -0300

    Update exemplo3.kt
    Inserção do código faltante.

--- 2_controle_de_fluxo/3_ranges/exemplo3.kt
+++ 2_controle_de_fluxo/3_ranges/exemplo3.kt
@@ -4,5 +4,13 @@
  * @see [Ranges](https://play.kotlinlang.org/byExample/02_control_flow/03_Ranges)
  */
 fun main() {
-    
- }
+    val x = 2
+    if (x in 1..5) {            // 1
+        print("x está no intervalo de 1 a 5")
+    }
+    println()
+
+    if (x !in 6..10) {          // 2
+        print("x não está no intervalo de 6 a 10")
+    }
+}
