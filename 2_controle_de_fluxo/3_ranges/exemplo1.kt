0559c5eb7c9436b1153fef62bebbb1634d3a85b9
Iago Vinicius Russi Novaes <34945729+iago010@users.noreply.github.com>
2023-09-19 18:16:12 -0300

    Update exemplo1.kt
    Inserção do código que estava faltando.

--- 2_controle_de_fluxo/3_ranges/exemplo1.kt
+++ 2_controle_de_fluxo/3_ranges/exemplo1.kt
@@ -4,5 +4,23 @@
  * @see [Ranges](https://play.kotlinlang.org/byExample/02_control_flow/03_Ranges)
  */
 fun main() {
-    
+    for(i in 0..3) {             // 1
+    print(i)
+    }
+    print(" ")
+
+    for(i in 0 until 3) {        // 2
+        print(i)
+    }
+    print(" ")
+
+    for(i in 2..8 step 2) {      // 3
+        print(i)
+    }
+    print(" ")
+
+    for (i in 3 downTo 0) {      // 4
+        print(i)
+    }
+    print(" ")
  }
